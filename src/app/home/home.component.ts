66414422e1b36f3620304781b6a08197b5bca697
Zenas Chen <czyczk@qq.com>
2018-07-11 13:44:30 +0800

    Fixed the problem of enums comparing to strings in HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,7 @@ import {Router} from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  adminEnum = AdminEnum;
   isCollapsed = false;
   triggerTemplate = null;
   @ViewChild('trigger') customTrigger: TemplateRef<void>;
